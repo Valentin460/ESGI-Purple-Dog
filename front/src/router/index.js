49950829912c2683c7774bf6900733c69927ff1c
Valentin460 <valentin.arthur1000@gmail.com>
2025-12-10 14:20:35 +0100

    Adding front of the dashboard

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ProfileView from '../views/ProfileView.vue'
 import FeedbackView from '../views/FeedbackView.vue'
+import AdminDashboardView from '../views/AdminDashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,14 @@ const router = createRouter({
     {
       path: '/avis',
       redirect: '/feedback'
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: AdminDashboardView,
+      meta: {
+        title: 'Dashboard Admin - Purple Dog'
+      }
     }
   ]
 })
