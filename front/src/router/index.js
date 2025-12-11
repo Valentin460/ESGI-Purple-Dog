4b2318dcfcbb011c2690bd3df4e9a8ce08d1d372
Honzikoi <honza.doan@gmail.com>
2025-12-11 12:07:13 +0100

    Quick front/router/index.js & back/package.json fix

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -79,6 +79,7 @@ const router = createRouter({
       name: 'PaymentSuccess',
       component: PaymentSuccess
     },
+    {
       path: '/admin',
       name: 'admin',
       component: AdminDashboardView,
@@ -86,76 +87,76 @@ const router = createRouter({
         title: 'Dashboard Admin - Purple Dog'
       }
     },
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue')
-  },
-  {
-    path: '/profile/userProfile',
-    name: 'UserProfile',
-    component: () => import('../views/Profile.vue')
-  },
-  {
-    path: '/auth/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
-  },
-  {
-    path: '/auth/register',
-    name: 'Register',
-    component: () => import('../views/RegisterParticular.vue')
-  },
-  {
-    path: '/auth/register/particulier',
-    name: 'RegisterParticulier',
-    component: () => import('../views/RegisterParticular.vue')
-  },
-  {
-    path: '/auth/register/professionnel',
-    name: 'RegisterProfessionnel',
-    component: () => import('../views/RegisterProfessional.vue')
-  },
-  {
-    path: '/auth/logout',
-    name: 'Logout',
-    component: () => import('../views/Logout.vue')
-  },
-  {
-    path: '/categories',
-    name: 'Categories',
-    component: () => import('../views/Categories.vue')
-  },
-  {
-    path: '/pro/cgv',
-    name: 'AcceptCGVProfessional',
-    component: () => import('../views/acceptCgvProfessional.vue')
-  },
-  {
-    path: '/pro/mandat',
-    name: 'CaApportProfessional',
-    component: () => import('../views/caApportProfessional.vue')
-  },
-  {
-    path: '/pro/newsletter',
-    name: 'NewsletterProfessional',
-    component: () => import('../views/NewsletterProfessional.vue')
-  },
-  {
-    path: '/pro/rgpd',
-    name: 'RgpdProfessional',
-    component: () => import('../views/RgpdProfessional.vue')
-  },
-  {
-    path: '/newsletter',
-    name: 'NewsletterParticular',
-    component: () => import('../views/NewsletterParticular.vue')
-  },
-  {
-    path: '/rgpd',
-    name: 'RgpdParticular',
-    component: () => import('../views/RgpdParticular.vue')
-  }
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('../views/Home.vue')
+    },
+    {
+      path: '/profile/userProfile',
+      name: 'UserProfile',
+      component: () => import('../views/Profile.vue')
+    },
+    {
+      path: '/auth/login',
+      name: 'Login',
+      component: () => import('../views/Login.vue')
+    },
+    {
+      path: '/auth/register',
+      name: 'Register',
+      component: () => import('../views/RegisterParticular.vue')
+    },
+    {
+      path: '/auth/register/particulier',
+      name: 'RegisterParticulier',
+      component: () => import('../views/RegisterParticular.vue')
+    },
+    {
+      path: '/auth/register/professionnel',
+      name: 'RegisterProfessionnel',
+      component: () => import('../views/RegisterProfessional.vue')
+    },
+    {
+      path: '/auth/logout',
+      name: 'Logout',
+      component: () => import('../views/Logout.vue')
+    },
+    {
+      path: '/categories',
+      name: 'Categories',
+      component: () => import('../views/Categories.vue')
+    },
+    {
+      path: '/pro/cgv',
+      name: 'AcceptCGVProfessional',
+      component: () => import('../views/acceptCgvProfessional.vue')
+    },
+    {
+      path: '/pro/mandat',
+      name: 'CaApportProfessional',
+      component: () => import('../views/caApportProfessional.vue')
+    },
+    {
+      path: '/pro/newsletter',
+      name: 'NewsletterProfessional',
+      component: () => import('../views/NewsletterProfessional.vue')
+    },
+    {
+      path: '/pro/rgpd',
+      name: 'RgpdProfessional',
+      component: () => import('../views/RgpdProfessional.vue')
+    },
+    {
+      path: '/newsletter',
+      name: 'NewsletterParticular',
+      component: () => import('../views/NewsletterParticular.vue')
+    },
+    {
+      path: '/rgpd',
+      name: 'RgpdParticular',
+      component: () => import('../views/RgpdParticular.vue')
+    }
   ]
 })
 
@@ -163,10 +164,5 @@ router.beforeEach((to, from, next) => {
   document.title = to.meta.title || 'Purple Dog'
   next()
 })
-
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes
-//   })
 
 export default router
